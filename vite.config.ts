4de364501dcdacce2b2dec009560acd98cb94442
Suraj Raccha <suraj@christiansentechsolutions.com>
2024-12-03 15:06:09 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,6 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  build: {
-    rollupOptions: {
-      input: 'src/main.tsx', // Your main entry file
-      output: {
-        // Specify the output directory and other options if needed
-      },
-      // Exclude the Excel file from the build
-      external: ['/work_hours.xlsx'],
-    },
-  },
+  }
 });
 
